609d164c16bb13dea14750c78d6be455d7ee9bc3
Lindsay Hayes <lhayes@hubbublab.com>
2022-06-02 13:32:41 -0600

    Updated the .gitignore to exclude some yarns.  Also removed a comment that was wrong.

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,7 +1,6 @@
 import 'dotenv/config';
 
 const Config = {
-  // Get the regular expression pattern from an environment variable or default to empty string if there is no environment variable
   CONTROL_CODE_REGEX: process.env.CONTROL_CODE_REGEX || '//(.*?)\n',
   DEFAULT_DELIMITER: process.env.DELIMITER || ',',
   ERRORS: {
